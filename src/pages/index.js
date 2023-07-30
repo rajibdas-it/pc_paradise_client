ca35703bbc299f3b739a395fb1444a94817b3c4c
rajibdas-it <rajibrad@gmail.com>
2023-07-30 21:23:12 +0600

    fetching products from json server

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,19 @@
+import Card from "@/Components/Card";
 import Header from "@/Components/Header";
 import Loading from "@/Components/ui/Loading";
 import RootLayout from "@/Layout/RootLayout";
 import React from "react";
 
-const HomePage = () => {
+const HomePage = ({ products }) => {
+  // console.log(products);
   return (
     <div className="container mx-auto">
       <Header />
+      <div className="mt-10 border border-red-800 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 ">
+        {products.map((product) => (
+          <Card product={product} />
+        ))}
+      </div>
     </div>
   );
 };
@@ -15,4 +22,15 @@ export default HomePage;
 
 HomePage.getLayout = function getLayout(page) {
   return <RootLayout>{page}</RootLayout>;
+};
+
+export const getStaticProps = async () => {
+  const res = await fetch("http://localhost:5000/products");
+  const data = await res.json();
+
+  return {
+    props: {
+      products: data,
+    },
+  };
 };
