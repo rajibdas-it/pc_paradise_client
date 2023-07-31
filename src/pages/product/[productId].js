bbfb1bcb720cf5200727ae41f697fe6a31e10d8f
rajibdas-it <rajibrad@gmail.com>
2023-08-01 01:36:32 +0600

    product details page done

--- src/pages/product/[productId].js
+++ src/pages/product/[productId].js
@@ -1,3 +1,4 @@
+import KeyFeatures from "@/Components/KeyFeatures";
 import RootLayout from "@/Layout/RootLayout";
 
 import React from "react";
@@ -6,34 +7,45 @@ const ProductDetails = ({ product }) => {
   console.log(product);
   return (
     <div className="p-5 mx-auto sm:p-10 md:p-16 ">
-      <div className="flex flex-col max-w-3xl mx-auto overflow-hidden rounded">
+      <div className="flex flex-col max-w-4xl mx-auto overflow-hidden rounded">
         <img
-          src="https://source.unsplash.com/random/480x360"
+          src={product?.image}
           alt=""
           className="w-full h-60 sm:h-96 dark:bg-gray-500"
         />
-        <div className="p-6 pb-12 m-4 mx-auto -mt-16 space-y-6 lg:max-w-2xl sm:px-10 sm:mx-12 lg:rounded-md dark:bg-gray-900">
+        <div className="p-6 pb-12 m-4 mx-auto -mt-16 space-y-6 lg:max-w-4xl sm:px-10 sm:mx-12 lg:rounded-md bg-gray-900">
           <div className="space-y-2">
             <a
               rel="noopener noreferrer"
               href="#"
               className="inline-block text-2xl font-semibold sm:text-3xl"
             >
-              The Best Activewear from the Nordstrom Anniversary Sale
+              {product?.product_name}
             </a>
-            <p className="text-xs dark:text-gray-400">
-              By
-              <a
-                rel="noopener noreferrer"
-                href="#"
-                className="text-xs hover:underline"
-              >
-                Leroy Jenkins
-              </a>
-            </p>
+            <div className="flex justify-start divide-x-2 divide-slate-200 space-x-5">
+              <p className="text-xs md:text-sm lg:text-xl text-gray-400">
+                Category: {product?.category}
+              </p>
+              <p className="pl-3 text-xs md:text-sm lg:text-xl text-gray-400">
+                Tk. {product.price}
+              </p>
+              <p className="text-xs md:text-sm lg:text-xl pl-3 text-gray-400">
+                Ratings: {product.rating}/5
+              </p>
+              <p className="text-xs md:text-sm lg:text-xl pl-3 text-gray-400">
+                Status: {product.status}
+              </p>
+            </div>
           </div>
-          <div className="dark:text-gray-100">
-            <p>Insert the actual text content here...</p>
+          <div className="text-gray-100 text-2xl">
+            <ul className="list-disc list-inside">
+              {product?.keyFeatures.map((feature, index) => (
+                <KeyFeatures key={index + 1} feature={feature} />
+              ))}
+            </ul>
+          </div>
+          <div className="text-gray-100 text-justify">
+            <p>{product?.description}</p>
           </div>
         </div>
       </div>
@@ -47,8 +59,8 @@ ProductDetails.getLayout = function getLayout(page) {
   return <RootLayout>{page}</RootLayout>;
 };
 
-export const getServerSideProps = async (context) => {
-  const { params } = context;
+export const getServerSideProps = async ({ params }) => {
+  // const { params } = context;
   const res = await fetch(
     `http://localhost:3000/api/products/${params.productId}`
   );
